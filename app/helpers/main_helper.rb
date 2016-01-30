b52927c7c972574fe8bd83d8e6e73f4deb3bae38
VoroninNick <voronin.nick@gmail.com>
2016-01-30 18:50:24 +0200

    Update main_helper.rb

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -7,8 +7,8 @@ module MainHelper
 
   def panel_with_header name, options = {}, &block
     raise Exception("please provide block") unless block_given?
-
+    options[:menu] = true if options[:menu].nil?
     #content_tag(name, options, nil, true, &block)
-    render("partials/panel_with_header.html.slim", { name: name, block: block }, &block)
+    render("partials/panel_with_header.html.slim", { name: name, options: options, block: block }, &block)
   end
 end
